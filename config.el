eb23f329d6a80deed1107a49c77b4ba6b5756187
M. Silk <silkm@student.unimelb.edu.au>
2020-11-04 09:42:18 +1100

    add winum shortcuts to overwrite switch projects

--- config.el
+++ config.el
@@ -67,3 +67,96 @@
 
 (after! evil-escape
   (setq evil-escape-key-sequence "jh"))
+
+;; Swiper
+;; (use-package! swiper)
+
+;; Org-publish
+;; (use-package! ox-publish)
+
+
+;; Winum
+(after! winum
+  ;; :init
+  ;; (map! "M-2" 'winum-select-window-2)
+  ;; (setq winum-keymap
+  ;;     (let ((map (make-sparse-keymap)))
+  ;;     (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+  ;;     (define-key map (kbd "M-1") 'winum-select-window-1)
+  ;;     (define-key map (kbd "M-2") 'winum-select-window-2)
+  ;;     (define-key map (kbd "M-3") 'winum-select-window-3)
+  ;;     (define-key map (kbd "M-4") 'winum-select-window-4)
+  ;;     (define-key map (kbd "M-5") 'winum-select-window-5)
+  ;;     (define-key map (kbd "M-6") 'winum-select-window-6)
+  ;;     (define-key map (kbd "M-7") 'winum-select-window-7)
+  ;;     (define-key map (kbd "M-8") 'winum-select-window-8)
+  ;;     map))
+  (setq winum-auto-setup-mode-line nil)
+  :config
+  (winum-mode))
+
+
+(cond ((eq system-type 'darwin)
+       (map! "s-0" #'winum-select-window-0-or-10)
+       (map! "s-1" #'winum-select-window-1)
+       (map! "s-2" #'winum-select-window-2)
+       (map! "s-3" #'winum-select-window-3)
+       (map! "s-4" #'winum-select-window-4)
+       (map! "s-5" #'winum-select-window-5)
+       (map! "s-6" #'winum-select-window-6)
+       (map! "s-7" #'winum-select-window-7)
+       (map! "s-8" #'winum-select-window-8)
+       (map! :after evil
+             :map evil-normal-state-map
+             "s-0" nil
+             "s-1" nil
+             "s-2" nil
+             "s-3" nil
+             "s-4" nil
+             "s-5" nil
+             "s-6" nil
+             "s-7" nil
+             "s-8" nil))
+      ((eq system-type 'gnu/linux)
+       (map! "M-0" #'winum-select-window-0-or-10)
+       (map! "M-1" #'winum-select-window-1)
+       (map! "M-2" #'winum-select-window-2)
+       (map! "M-3" #'winum-select-window-3)
+       (map! "M-4" #'winum-select-window-4)
+       (map! "M-5" #'winum-select-window-5)
+       (map! "M-6" #'winum-select-window-6)
+       (map! "M-7" #'winum-select-window-7)
+       (map! "M-8" #'winum-select-window-8)
+       (map! :after evil
+             :map evil-normal-state-map
+             "M-0" nil
+             "M-1" nil
+             "M-2" nil
+             "M-3" nil
+             "M-4" nil
+             "M-5" nil
+             "M-6" nil
+             "M-7" nil
+             "M-8" nil)))
+
+;; (map! "s-0" #'winum-select-window-0-or-10)
+;; (map! "s-1" #'winum-select-window-1)
+;; (map! "s-2" #'winum-select-window-2)
+;; (map! "s-3" #'winum-select-window-3)
+;; (map! "s-4" #'winum-select-window-4)
+;; (map! "s-5" #'winum-select-window-5)
+;; (map! "s-6" #'winum-select-window-6)
+;; (map! "s-7" #'winum-select-window-7)
+;; (map! "s-8" #'winum-select-window-8)
+
+;; (map! :after evil
+;;       :map evil-normal-state-map
+;;       "s-0" nil
+;;       "s-1" nil
+;;       "s-2" nil
+;;       "s-3" nil
+;;       "s-4" nil
+;;       "s-5" nil
+;;       "s-6" nil
+;;       "s-7" nil
+;;       "s-8" nil)
