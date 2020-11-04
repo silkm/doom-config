54a210da81f5dfecaa792dd4652cc84283209545
M. Silk <silkm@student.unimelb.edu.au>
2020-11-05 09:50:52 +1100

    fix ESS config

--- config.el
+++ config.el
@@ -77,20 +77,6 @@
 
 ;; Winum
 (after! winum
-  ;; :init
-  ;; (map! "M-2" 'winum-select-window-2)
-  ;; (setq winum-keymap
-  ;;     (let ((map (make-sparse-keymap)))
-  ;;     (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
-  ;;     (define-key map (kbd "M-1") 'winum-select-window-1)
-  ;;     (define-key map (kbd "M-2") 'winum-select-window-2)
-  ;;     (define-key map (kbd "M-3") 'winum-select-window-3)
-  ;;     (define-key map (kbd "M-4") 'winum-select-window-4)
-  ;;     (define-key map (kbd "M-5") 'winum-select-window-5)
-  ;;     (define-key map (kbd "M-6") 'winum-select-window-6)
-  ;;     (define-key map (kbd "M-7") 'winum-select-window-7)
-  ;;     (define-key map (kbd "M-8") 'winum-select-window-8)
-  ;;     map))
   (setq winum-auto-setup-mode-line nil)
   :config
   (winum-mode))
@@ -139,24 +125,38 @@
              "M-7" nil
              "M-8" nil)))
 
-;; (map! "s-0" #'winum-select-window-0-or-10)
-;; (map! "s-1" #'winum-select-window-1)
-;; (map! "s-2" #'winum-select-window-2)
-;; (map! "s-3" #'winum-select-window-3)
-;; (map! "s-4" #'winum-select-window-4)
-;; (map! "s-5" #'winum-select-window-5)
-;; (map! "s-6" #'winum-select-window-6)
-;; (map! "s-7" #'winum-select-window-7)
-;; (map! "s-8" #'winum-select-window-8)
+;; ESS config
+(after! ess
+  :init
+  (setq ess-indent-with-fancy-comments nil
+        ess-style 'RStudio
+        ess-ask-for-ess-directory nil
+        inferior-R-args "--no-save")
+  (map! :map ess-mode-map
+        :nv "<C-return>" #'ess-eval-line-and-step))
 
-;; (map! :after evil
-;;       :map evil-normal-state-map
-;;       "s-0" nil
-;;       "s-1" nil
-;;       "s-2" nil
-;;       "s-3" nil
-;;       "s-4" nil
-;;       "s-5" nil
-;;       "s-6" nil
-;;       "s-7" nil
-;;       "s-8" nil)
+
+;; ESS config
+;; (add-hook! ess
+;;   (setq ess-indent-with-fancy-comments nil)
+;;   (setq ess-r-flymake-linters
+;; 	'("assignment_linter=NULL"
+;; 	  "closed_curly_linter=NULL"
+;; 	  "commas_linter=NULL"
+;; 	  "commented_code_linter=NULL"
+;; 	  "line_length_linter=NULL"
+;; 	  "object_usage_linter=NULL"
+;; 	  "camel_case_linter=NULL"
+;; 	  "snake_case_linter=NULL"
+;; 	  "object_name_linter=NULL"
+;; 	  "spaces_inside_linter=NULL"
+;; 	  "spaces_left_parentheses_linter=NULL"
+;; 	  ;; "trailing_blank_lines_linter=NULL"
+;; 	  ;; "trailing_whitespace_linter=NULL"
+;; 	  ))
+;;   (setq inferior-R-args "--no-save")
+;;   (setq ess-set-style 'RStudio)
+;;   (setq ess-startup-directory "/storage/data/blackhole/")
+;;   (setq ess-ask-for-ess-directory nil)
+;;   (setq ess-use-company t))
+;;   ;; (setq ess-tab-complete-in-script t))
