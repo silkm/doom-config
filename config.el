d167158e26b53f98d294900649cc00523b3a5a1e
M. Silk <silk.michael1@gmail.com>
2021-11-01 12:25:42 +1100

    add terminal-specific winum config

--- config.el
+++ config.el
@@ -177,6 +177,18 @@
              "M-7" nil
              "M-8" nil)))
 
+(when (not display-graphic-p)
+  (map! :leader
+        "0" 'winum-select-window-0-or-10
+        "1" #'winum-select-window-1
+        "2" #'winum-select-window-2
+        "3" #'winum-select-window-3
+        "4" #'winum-select-window-4
+        "5" #'winum-select-window-5
+        "6" #'winum-select-window-6
+        "7" #'winum-select-window-7
+        "8" #'winum-select-window-8))
+
 
 (after! ess
   :init
