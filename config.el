472e1f2c79c6656dd97641e72fc242a5317601e0
M. Silk <silk.michael1@gmail.com>
2022-05-17 17:40:33 +1000

    update config.el, edit ess lintrs, add rjsx mode

--- config.el
+++ config.el
@@ -9,34 +9,54 @@
 (setq user-full-name "M. Silk"
       user-mail-address "silk.michael1@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/notebook/")
 
-;; Here are some additional functions/macros that could help you configure Doom:
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -49,6 +69,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
@@ -122,16 +144,18 @@
            "* TODO %i%?\n")
           ("l" "Maintenance Log" entry (file+headline "~/notebook/notes.org" "Maintenance")
            "* %^{PROMPT} %^g \n%u\n\n%?\n"
-           :empty-lines 1)
-          ("a" "Avocado Log" entry (file+headline "~/notebook/avocado.org" "Measurements")
-           "* %U %?\n")))
+           :empty-lines 1)))
   (setq org-refile-targets '(("~/notebook/notes.org" :maxlevel . 3)))
   (setq org-refile-allow-creating-parent-nodes 'confirm)
   (add-hook 'org-mode-hook
             (lambda ()
               (make-local-variable 'company-idle-delay)
-              (setq company-idle-delay 999))))
+              (setq company-idle-delay 999)))
+  (add-to-list 'org-src-lang-modes (cons "jsx" 'rjsx-mode)))
 
+
+
+(use-package! ox-ipynb)
 
 (after! winum
   (setq winum-auto-setup-mode-line nil)
@@ -200,14 +224,36 @@
   (setq ess-indent-with-fancy-comments nil
         ess-style 'RStudio
         ess-ask-for-ess-directory nil
+        ess-history-directory "~/.cache"
         inferior-R-args "--no-save")
   (map! :map ess-mode-map
         :nv "<C-return>" #'ess-eval-line-and-step)
   :config
   (setq flycheck-lintr-linters
         (concat "with_defaults(line_length_linter(120), "
-                 "object_name_linter=NULL, "
-                 "assignment_linter=NULL)")))
+                "object_usage_linter=NULL)"))
+  (defun flycheck-custom-simple-linters ()
+    (interactive)
+    (setq flycheck-lintr-linters
+          (concat "with_defaults(line_length_linter(120), "
+                  "object_usage_linter=NULL)")))
+  (defun flycheck-custom-strict-linters ()
+    (interactive)
+    (setq flycheck-lintr-linters
+          (concat "with_defaults(line_length_linter(120), "
+                  "absolute_path_linter, "
+                  "nonportable_path_linter, "
+                  "camel_case_linter, "
+                  "extraction_operator_linter, "
+                  "implicit_integer_linter, "
+                  "paren_brace_linter, "
+                  "semicolon_terminator_linter, "
+                  "todo_comment_linter, "
+                  "T_and_F_symbol_linter, "
+                  "undesirable_function_linter, "
+                  "undesirable_operator_linter, "
+                  "unneeded_concatenation_linter)"))))
+
 
 ;; Add path for LaTeX
 (setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin/"))
