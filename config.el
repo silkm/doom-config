b700dcf2a892be86a7f2e3aec3daeabed71282e0
silkm <michael.silk@populationgenomics.org.au>
2025-05-13 10:59:31 +1000

    test some other themes

--- config.el
+++ config.el
@@ -32,8 +32,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-nord)
+(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-nord)
+;; (setq doom-theme 'doom-vibrant)
+;; (setq doom-theme 'apropospriate-dark)
+;; (setq doom-theme 'doom-monokai-pro)
 ;; (setq doom-theme 'doom-opera)
 
 
