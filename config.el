019437f95497f0555c505642c1d1c556a280bf1f
silkm <michael.silk@populationgenomics.org.au>
2025-08-22 22:03:25 +1000

    fix unused variables in my org insert header cmd

--- config.el
+++ config.el
@@ -384,43 +384,43 @@
            ;; Position determines where org-insert-todo-heading and `org-insert-item'
            ;; insert the new list item.
            (if (eq direction 'above)
-               (org-beginning-of-item)
-             (end-of-line))
+             (org-beginning-of-item)
+           (end-of-line))
            (let* ((ctx-item? (eq 'item (org-element-type context)))
-                  (ctx-cb (org-element-property :contents-begin context))
-                  ;; Hack to handle edge case where the point is at the
-                  ;; beginning of the first item
-                  (beginning-of-list? (and (not ctx-item?)
-                                           (= ctx-cb orig-point)))
-                  (item-context (if beginning-of-list?
-                                    (org-element-context)
-                                  context))
-                  ;; Horrible hack to handle edge case where the
-                  ;; line of the bullet is empty
-                  (ictx-cb (org-element-property :contents-begin item-context))
-                  (empty? (and (eq direction 'below)
-                               ;; in case contents-begin is nil, or contents-begin
-                               ;; equals the position end of the line, the item is
-                               ;; empty
-                               (or (not ictx-cb)
-                                   (= ictx-cb
-                                      (1+ (point))))))
-                  (pre-insert-point (point)))
-             ;; Insert dummy content, so that `org-insert-item'
-             ;; inserts content below this item
-             (when empty?
-               (insert " "))
-             (org-insert-item (org-element-property :checkbox context))
-             ;; Remove dummy content
-             (when empty?
-               (delete-region pre-insert-point (1+ pre-insert-point))))))
+                (ctx-cb (org-element-property :contents-begin context))
+                ;; Hack to handle edge case where the point is at the
+                ;; beginning of the first item
+                (beginning-of-list? (and (not ctx-item?)
+                                         (= ctx-cb orig-point)))
+                (item-context (if beginning-of-list?
+                                  (org-element-context)
+                                context))
+                ;; Horrible hack to handle edge case where the
+                ;; line of the bullet is empty
+                (ictx-cb (org-element-property :contents-begin item-context))
+                (empty? (and (eq direction 'below)
+                             ;; in case contents-begin is nil, or contents-begin
+                             ;; equals the position end of the line, the item is
+                             ;; empty
+                             (or (not ictx-cb)
+                                 (= ictx-cb
+                                    (1+ (point))))))
+                (pre-insert-point (point)))
+           ;; Insert dummy content, so that `org-insert-item'
+           ;; inserts content below this item
+           (when empty?
+             (insert " "))
+           (org-insert-item (org-element-property :checkbox context))
+           ;; Remove dummy content
+           (when empty?
+             (delete-region pre-insert-point (1+ pre-insert-point))))))
         ;; Add a new table row
         ((or `table `table-row)
          (pcase direction
            ('below (save-excursion (org-table-insert-row t))
-                   (org-table-next-row))
+                 (org-table-next-row))
            ('above (save-excursion (org-shiftmetadown))
-                   (+org/table-previous-row))))
+                 (+org/table-previous-row))))
         ;; Otherwise, add a new heading, carrying over any todo state, if
         ;; necessary.
         (_
@@ -430,40 +430,39 @@
            ;; position. It's simpler to express this command's responsibility at a
            ;; lower level than work around all the quirks in org's API.
            (pcase direction
-             (`below
-              (let (org-insert-heading-respect-content)
-                (goto-char (line-end-position))
-                (org-end-of-subtree)
-                ;; Check if we're about to create a TODO heading
-                (let* ((todo-keyword (org-element-property :todo-keyword context))
-                       (todo-type (org-element-property :todo-type context))
-                       (will-be-todo (and todo-keyword (not (eq todo-type 'done)))))
-                  ;; Only ensure newline above new heading if it won't be a TODO
-                  (unless (or will-be-todo
-                              (looking-back "\n\n" (max (point-min) (- (point) 2))))
-                    (insert "\n")))
-                (insert "\n" (make-string level ?*) " ")))
-             (`above
-              (org-back-to-heading)
-              ;; Check if we're about to create a TODO heading
-              (let* ((todo-keyword (org-element-property :todo-keyword context))
-                     (todo-type (org-element-property :todo-type context))
-                     (will-be-todo (and todo-keyword (not (eq todo-type 'done)))))
-                ;; Only ensure newline above new heading if it won't be a TODO
-                (unless (or will-be-todo
-                            (looking-back "\n\n" (max (point-min) (- (point) 2))))
-                  (insert "\n")))
-              (insert (make-string level ?*) " ")
-              (save-excursion (insert "\n"))))
+           (`below
+            (goto-char (line-end-position))
+            (org-end-of-subtree)
+            ;; Check if we're about to create a TODO heading
+            (let* ((todo-keyword (org-element-property :todo-keyword context))
+                   (todo-type (org-element-property :todo-type context))
+                   (will-be-todo (and todo-keyword (not (eq todo-type 'done)))))
+              ;; Only ensure newline above new heading if it won't be a TODO
+              (unless (or will-be-todo
+                          (looking-back "\n\n" (max (point-min) (- (point) 2))))
+                (insert "\n")))
+            (insert "\n" (make-string level ?*) " "))
+           (`above
+            (org-back-to-heading)
+            ;; Check if we're about to create a TODO heading
+            (let* ((todo-keyword (org-element-property :todo-keyword context))
+                   (todo-type (org-element-property :todo-type context))
+                   (will-be-todo (and todo-keyword (not (eq todo-type 'done)))))
+              ;; Only ensure newline above new heading if it won't be a TODO
+              (unless (or will-be-todo
+                          (looking-back "\n\n" (max (point-min) (- (point) 2))))
+                (insert "\n")))
+            (insert (make-string level ?*) " ")
+            (save-excursion (insert "\n"))))
            (run-hooks 'org-insert-heading-hook)
            (when-let* ((todo-keyword (org-element-property :todo-keyword context))
-                       (todo-type    (org-element-property :todo-type context)))
-             (org-todo
-              (cond ((eq todo-type 'done)
-                     ;; Doesn't make sense to create more "DONE" headings
-                     (car (+org-get-todo-keywords-for todo-keyword)))
-                    (todo-keyword)
-                    ('todo)))))))
+                     (todo-type    (org-element-property :todo-type context)))
+           (org-todo
+            (cond ((eq todo-type 'done)
+                   ;; Doesn't make sense to create more "DONE" headings
+                   (car (+org-get-todo-keywords-for todo-keyword)))
+                  (todo-keyword)
+                  ('todo)))))))
       (when (org-invisible-p)
         (org-show-hidden-entry))
       (when (and (bound-and-true-p evil-local-mode)
@@ -591,9 +590,6 @@
             "\n\n\n\nlibrary(data.table)\nlibrary(magrittr)\nlibrary(purrr)\n")
     (goto-char (point-min))
     (forward-line 4))
-  :defer
-  ;; Fix R:ESS comments going grey and unreadable
-  ;; (setq-local ansi-color-for-comint-mode 'filter)
   (setq ess-indent-with-fancy-comments nil
         ess-style 'RStudio
         inferior-R-args "--no-save")
